d6fd0d129b9b54cd9106cf69cb6fcf0ce40ea494
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-15 06:50:42 -0300

    clean market index

--- data-cleaning/subset_data.py
+++ data-cleaning/subset_data.py
@@ -34,8 +34,9 @@ from pathlib import Path
 folder_path = Path(__file__).parent.parent / 'data' 
 file_list = ['bovespa_indexes.csv', 'bovespa_stocks.csv']
 stocks_df = pd.read_csv(folder_path / file_list[1])
+market_index_df = pd.read_csv(folder_path / file_list[0])
 
-# Clean the data
+# Clean the stock data
 stocks_df['Date'] = stocks_df['Date'].astype(str)\
 .apply(lambda x: re.search(r'\d{4}-\d{2}-\d{2}', x)\
 .group(0))
@@ -62,10 +63,34 @@ stocks_df = stocks_df.dropna(
 stocks_df = stocks_df.ffill().bfill()
 stocks_df = stocks_df.pct_change()
 stocks_df = stocks_df.dropna()
-stocks_df = (stocks_df - stocks_df.mean()) / stocks_df.std()
 
-# Save the data
-stocks_df.to_csv(folder_path / 'stocks_cleaned.csv')
+stocks_normalized_df = (stocks_df - stocks_df.mean()) / stocks_df.std()
+stocks_normalized_df.to_csv(folder_path / 'stocks_normalized.csv')
 stocks_df.columns.to_series().to_csv(
     folder_path / 'stocks_tickers.csv', index=False
 )
+
+# Clean the market index data
+market_index_df['Date'] = market_index_df['Date'].astype(str)\
+.apply(lambda x: re.search(r'\d{4}-\d{2}-\d{2}', x)\
+.group(0))
+market_index_df['Date'] = pd.to_datetime(market_index_df['Date'],
+    format='%Y-%m-%d')
+market_index_df = market_index_df[
+    market_index_df['Symbol'] == '^BVSP'
+]
+market_index_df = market_index_df[['Date', 'Adj Close']]
+market_index_df = market_index_df.rename(
+    columns={'Adj Close': 'index',
+    'Date': 'date'}
+)
+market_index_df.set_index('date', inplace=True)
+market_index_df.dropna(inplace=True)
+market_index_df = market_index_df.pct_change()
+
+# find the intersection of the two dataframes indexes
+intersection_dates = stocks_df.index.intersection(
+    market_index_df.index
+)
+stocks_df = stocks_df.loc[intersection_dates]
+market_index_df = market_index_df.loc[intersection_dates]
