a31fe38343f3a265b05ef5b4c438c860287be610
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-16 06:02:05 -0300

    round values for better visualization

--- graph_construction/visualization.py
+++ graph_construction/visualization.py
@@ -1,10 +1,13 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def visualize_graph(graph):
+def visualize_graph(graph, round_values=False):
     G = nx.Graph()
     for edge in graph.edge_list:
-        G.add_edge(edge[0], edge[1], weight=edge[2])
+        if round_values:
+            G.add_edge(edge[0], edge[1], weight=round(edge[2], 2))
+        else:
+            G.add_edge(edge[0], edge[1], weight=edge[2])
     pos = nx.spring_layout(G)
     labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
