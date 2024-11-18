355f25e471259ddaa40fd6acbb82b06742dd697c
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-18 12:29:49 -0300

    feat get embeddings

---
+++ graph_embeddings/embeddings.py
@@ -0,0 +1,49 @@
+from torch_geometric.nn import Node2Vec
+import torch 
+import numpy as np 
+
+class Embeddings:
+    def __init__(self, model, training_data, **kwargs):
+        self.kwargs = kwargs
+        self.training_data = training_data
+        self.get_model(model)
+        
+
+    def get_model(self, model):
+        if model.lower() == 'node2vec':
+            self.model = Node2Vec(
+                edge_index = self.training_data.edge_index,
+                embedding_dim = self.kwargs.get('embedding_dim', 128),
+                walk_length = self.kwargs.get('walk_length', 80),
+                context_size = self.kwargs.get('context_size', 20),
+                walks_per_node = self.kwargs.get('walks_per_node', 10),
+                num_negative_samples = self.kwargs.get('num_negative_samples', 1),
+                p = self.kwargs.get('p', 1),
+                q = self.kwargs.get('q', 1),
+                sparse=self.kwargs.get('sparse', True),
+            )
+        else:
+            raise NotImplementedError(f"Model {model} not implemented")
+
+    def train(self):
+        loader = self.model.loader(batch_size=128, shuffle=True)
+        self.model.train()
+        optimizer = torch.optim.SparseAdam(list(self.model.parameters()), lr=0.01)
+        total_loss = 0
+        for positive_random_walks, negative_random_walks in loader:
+            optimizer.zero_grad()
+            loss = self.model.loss(positive_random_walks, negative_random_walks)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+        return total_loss/len(loader)
+
+    def get_embeddings(self):
+        self.model.eval()
+        with torch.no_grad():
+            all_embeddings = self.model(
+                torch.arange(
+                    self.training_data.edge_index.max()+1))
+
+        embeddings_np = all_embeddings.numpy()
+        return embeddings_np, self.training_data.node_mapping
--- tests/test_embeddings.py
+++ tests/test_embeddings.py
@@ -11,6 +11,7 @@ class RandomData:
             [0, 1, 2, 1],  # source nodes
             [1, 0, 1, 2]  # target nodes
         ])
+        self.node_mapping = {'PETR4': 0, 'VALE3': 1, 'ITUB4': 2}
 
 @pytest.fixture
 def data():
@@ -27,5 +28,14 @@ def node2vec(data):
         walks_per_node=10)
 
 
-def test_node2vec(node2vec):
+def test_initialization(node2vec):
     assert node2vec.model.__class__.__name__ == 'Node2Vec'
+
+def test_train(node2vec):
+    loss_percent = node2vec.train()
+    assert loss_percent > 0
+
+def test_get_embeddings(node2vec):
+    embeddings, node_mapping = node2vec.get_embeddings()
+    assert embeddings.shape == (3, 128)
+    assert node_mapping == {'PETR4': 0, 'VALE3': 1, 'ITUB4': 2}
