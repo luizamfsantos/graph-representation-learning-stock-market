3d08d573cdc572bc202030e5f447ebb80cc3c8e5
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-18 18:36:59 -0300

    graph different levels vs clusters

--- evaluation/loss_visualization.py
+++ evaluation/loss_visualization.py
@@ -6,7 +6,7 @@ from pathlib import Path
 folder_path = Path(__file__).parent.parent / 'data'
 df = pd.read_csv(folder_path / 'loss_results.csv')
 
-# Plot
+# Plot Loss Percentage vs Number of Clusters for Different rho_min
 # plt.rcParams['text.usetex'] = True
 plt.figure(figsize=(10, 6))
 for rho, group in df.groupby('rho_min'):
@@ -16,6 +16,19 @@ plt.title("Loss Percentage vs Number of Clusters for Different rho_min")
 plt.xlabel("Number of Clusters")
 plt.ylabel("Loss Percentage")
 plt.legend(title="rho_min")
+plt.grid(visible=True, linestyle='--', alpha=0.6)
+plt.tight_layout()
+plt.show()
+
+# Plot Loss Percentage vs Different rho_min for different Number of Clusters
+plt.figure(figsize=(10, 6))
+for n_clusters, group in df.groupby('n_clusters'):
+    plt.plot(group['rho_min'], group['loss_percentage'], marker='', label=f"n_clusters = {n_clusters}")
+
+plt.title("Loss Percentage at different levels for different number of clusters")
+plt.xlabel("Rho_min")
+plt.ylabel("Loss Percentage")
+plt.legend(title="n_clusters")
 plt.grid(visible=True, linestyle='--', alpha=0.6)
 plt.tight_layout()
 plt.show()
