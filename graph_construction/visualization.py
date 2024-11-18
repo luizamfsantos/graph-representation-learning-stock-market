d143bdfdc2a025a4648689a1a84fb1e379a341cd
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-18 08:32:16 -0300

    separate create networkx logic so it can be reused

--- graph_construction/visualization.py
+++ graph_construction/visualization.py
@@ -1,13 +1,17 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def visualize_graph(graph, round_values=False):
+def networkx_graph(graph, round_values=False):
     G = nx.Graph()
     for edge in graph.edge_list:
         if round_values:
             G.add_edge(edge[0], edge[1], weight=round(edge[2], 2))
         else:
             G.add_edge(edge[0], edge[1], weight=edge[2])
+    return G
+
+def visualize_graph(graph, round_values=False):
+    G = networkx_graph(graph, round_values)
     pos = nx.spring_layout(G)
     labels = nx.get_edge_attributes(G, 'weight')
     nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
